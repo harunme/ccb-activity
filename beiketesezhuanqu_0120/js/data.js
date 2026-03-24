9d9504604d9712905a4baa4c07ddc7bcaf7335e8
BIN WANG <binwang@BINdeMacBook-Pro.local>
2026-03-24 15:31:27 +0800

    Initial commit: 贝壳特色专区 + 客户经理专区 assets

---
+++ beiketesezhuanqu_0120/js/data.js
@@ -0,0 +1,83 @@
+﻿let linkArr = [
+    { 
+      eleName: '特色房源',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://marketing.ke.com/activity/h5/114452?notChangeCity=1&activityId=114452&utm_source=wailian&utm_medium=yingxiaohuodong_yinhangapp1',
+      //手机银行端内跳转链接
+      mbsJumpUrl: '',
+      //微信跳转链接
+      wxJumpUrl: '',
+    },
+    { 
+      eleName: '贷款服务',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://res.yunbusiness.ccb.com/service/resource/mng/quickTopicsPage/2026/202601/20260119/20260119070000000053/d11840f6-b55e-44f6-aa07-68fa1b823518/index.html',
+      //手机银行端内跳转链接
+      mbsJumpUrl: 'https://i.xiu.ccb.com/v3/q/ezdk7p/',
+      //微信跳转链接
+      wxJumpUrl: 'https://i.xiu.ccb.com/v3/q/ezdk7p/',
+    },
+    { 
+      eleName: '优选商户',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://syx6.dmsp.ccb.com/ccbact/m348/DP21122601072208TY90-act.html',
+      //手机银行端内跳转链接
+      mbsJumpUrl: '',
+      //微信跳转链接
+      wxJumpUrl: '',
+    },
+    { 
+      eleName: '圣都装修',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://res.yunbusiness.ccb.com/service/resource/mng/quickTopicsPage/2026/202601/20260109/20260109080000000009/45fa7702-4133-415c-8b7a-1b24e78aaf7d/index.html',
+      //手机银行端内跳转链接
+      mbsJumpUrl: '',
+      //微信跳转链接
+      wxJumpUrl: '',
+    },
+    { 
+      eleName: '消费贷享贴息',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://res.yunbusiness.ccb.com/service/resource/mng/quickTopicsPage/2025/202509/20250902/20250902090000000102/0a02624c-1a25-4879-b944-a858e84bba41/index.html',
+      //手机银行端内跳转链接
+      mbsJumpUrl: 'https://i.xiu.ccb.com/v3/m/anhnzp/',
+      //微信跳转链接
+      wxJumpUrl: 'https://i.xiu.ccb.com/v3/m/anhnzp/',
+    },
+    { 
+      eleName: '装修分期',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://yunbusiness.ccb.com/gbchannel/e_report/CCBLIFE/#/decoInstallment?fromPage=webview',
+      //手机银行端内跳转链接
+      mbsJumpUrl: 'https://mobile.ccb.com/e_report/outlink/index.html?link_id=4678e70e0f7e411eb4ee5de139e6b684',
+      //微信跳转链接
+      wxJumpUrl: '',
+    },
+    { 
+      eleName: '消费贷款',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://i.xiu.ccb.com/v3/m/8kjbu3/',
+      //手机银行端内跳转链接
+      mbsJumpUrl: '',
+      //微信跳转链接
+      wxJumpUrl: '',
+    },
+    { 
+      eleName: '龙钱宝1号',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://res.yunbusiness.ccb.com/service/resource/mng/quickTopicsPage/2025/202511/20251127/20251127080000000006/05013e6c-1f24-4aee-9917-e37e2ffec966/index.html',
+      //手机银行端内跳转链接
+      mbsJumpUrl: 'https://i.xiu.ccb.com/v3/q/jpvxsg/',
+      //微信跳转链接
+      wxJumpUrl: '',
+    },
+    { 
+      eleName: '存款优选专区',
+      //建行生活端内跳转链接
+      jumpUrl: 'https://res.yunbusiness.ccb.com/gbchannel/e_report/CloudLink/index.html?target=url&ccbParam=https%3A%2F%2Fres.yunbusiness.ccb.com%2Fgbchannel%2Fe_report%2FCCBLIFE%2F%23%2FinterLayer%2FYS44000008001147%3FLOGIN_REQUIRED%3D1%26path%3D%2Fv2%2Fmanage%2Fbook%2Fznnpvg%2F',
+      //手机银行端内跳转链接
+      mbsJumpUrl: 'https://i.xiu.ccb.com/v3/q/me4d66/',
+      //微信跳转链接
+      wxJumpUrl: 'https://i.xiu.ccb.com/v3/q/me4d66/',
+    },
+]
