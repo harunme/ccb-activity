2e9b71c259047aa04c866ce60b528944b9978b69
harun <yowangbin@gmail.com>
2026-03-30 10:58:37 +0800

    Add nuojiatesezhuanqu_20260330 section

---
+++ nuojiatesezhuanqu_20260330/js/data.js
@@ -0,0 +1,90 @@
+﻿let linkArr = [
+  {
+    //app端内拉起小程序
+    eleName: "特色房源",
+    jumpUrl: "",
+    couponType: "jumpMini",
+    miniId: "gh_327bec485b01", //填其他小程序的原始id，若跳建行生活小程序, 就删去这行
+    pagesUrl: "pages/index/index.html", //小程序路径
+  },
+  {
+    eleName: "贷款服务",
+    //建行生活端内跳转链接
+    jumpUrl:
+      "https://res.yunbusiness.ccb.com/service/resource/mng/quickTopicsPage/2026/202601/20260119/20260119070000000053/d11840f6-b55e-44f6-aa07-68fa1b823518/index.html",
+    //手机银行端内跳转链接
+    mbsJumpUrl: "",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+  {
+    eleName: "优选商户",
+    //建行生活端内跳转链接
+    jumpUrl:
+      "https://res.yunbusiness.ccb.com/gbchannel/e_report/CCBLIFE/#/interLayer/YS44000010000078?LOGIN_REQUIRED=0&path=/e/ccb_gjb/vaMlvuVAJjy6u0c5XnRCRA",
+    //手机银行端内跳转链接
+    mbsJumpUrl: "",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+  {
+    eleName: "精选装修",
+    //建行生活端内跳转链接
+    jumpUrl:
+      "https://res.yunbusiness.ccb.com/gbchannel/e_report/CCBLIFE/#/interLayer/YS44000010000078?LOGIN_REQUIRED=0&path=/e/ccb_gjb/uQw9tV4fI1Oy_LIjtcZMSg",
+    //手机银行端内跳转链接
+    mbsJumpUrl: "",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+  {
+    eleName: "消费贷享贴息",
+    //建行生活端内跳转链接
+    jumpUrl:
+      "https://res.yunbusiness.ccb.com/service/resource/mng/quickTopicsPage/2025/202509/20250902/20250902090000000102/0a02624c-1a25-4879-b944-a858e84bba41/index.html",
+    //手机银行端内跳转链接
+    mbsJumpUrl: "",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+  {
+    eleName: "装修分期",
+    //建行生活端内跳转链接
+    jumpUrl:
+      "https://res.yunbusiness.ccb.com/gbchannel/e_report/CCBLIFE/#/decoInstallment?fromPage=webview",
+    //手机银行端内跳转链接
+    mbsJumpUrl:
+      "https://mobile.ccb.com/e_report/outlink/index.html?link_id=4678e70e0f7e411eb4ee5de139e6b684",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+  {
+    eleName: "消费贷款",
+    //建行生活端内跳转链接
+    jumpUrl: "https://i.xiu.ccb.com/v3/m/8kjbu3/",
+    //手机银行端内跳转链接
+    mbsJumpUrl: "",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+  {
+    eleName: "龙钱宝1号",
+    //建行生活端内跳转链接
+    jumpUrl:
+      "https://res.yunbusiness.ccb.com/service/resource/mng/quickTopicsPage/2025/202511/20251127/20251127080000000006/05013e6c-1f24-4aee-9917-e37e2ffec966/index.html",
+    //手机银行端内跳转链接
+    mbsJumpUrl: "",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+  {
+    eleName: "存款优选专区",
+    //建行生活端内跳转链接
+    jumpUrl:
+      "https://res.yunbusiness.ccb.com/gbchannel/e_report/CloudLink/index.html?target=url&ccbParam=https%3A%2F%2Fres.yunbusiness.ccb.com%2Fgbchannel%2Fe_report%2FCCBLIFE%2F%23%2FinterLayer%2FYS44000008001147%3FLOGIN_REQUIRED%3D1%26path%3D%2Fv2%2Fmanage%2Fbook%2Fznnpvg%2F",
+    //手机银行端内跳转链接
+    mbsJumpUrl: "",
+    //微信跳转链接
+    wxJumpUrl: "",
+  },
+];
